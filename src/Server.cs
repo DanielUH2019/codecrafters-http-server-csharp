1a81dd929150a55e6ef5abf1bcec1c8effedd69a
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-06-25 17:01:18 -0400

    pass stage 2

--- src/Server.cs
+++ src/Server.cs
@@ -1,7 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 
 var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+socket.Send(Encoding.UTF8.GetBytes(GetSuccessfulResponse("HTTP/1.1", 200)));
+
+
+
+static string GetSuccessfulResponse(string httpVersion, int statusCode, string phrase = "OK", string headers = "", string body = "")
+{
+    var response = new StringBuilder($"{httpVersion} {statusCode} {phrase}\r\n");
+    response.Append($"{headers}\r\n");
+    response.Append(body);
+    return response.ToString();
+}
